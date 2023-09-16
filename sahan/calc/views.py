b05d844551f3364649d0c91aaac802cc2243cf21
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-09-16 13:07:31 +0530

    Update views.py

--- sahan/calc/views.py
+++ sahan/calc/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{'name':'Sahan'}) #html page path
+    return render(request,'home.html',{'name':'Sahan'}) #html page path , name is dynamic
