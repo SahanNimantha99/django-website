de1d0d63812a4902c3765d04241abed3771d0027
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-09-16 13:42:40 +0530

    Update urls.py

--- sahan/calc/urls.py
+++ sahan/calc/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .import views
 
 urlpatterns =[
-    path('',views.home,name='home')#path of what to view
+    path('',views.home,name='home'),#path of what to view
+    path('add',views.add,name='add')
 ]
