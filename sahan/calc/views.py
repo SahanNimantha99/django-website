106ca1e97bb082c906e8e71daada3d05ca28766a
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-09-19 10:32:52 +0530

    Update

--- sahan/calc/views.py
+++ sahan/calc/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 def add(request):
 
-    val1 = int(request.POST['num1'])
-    val2 = int(request.POST['num2'])
+    val1 = int(request.POST['num1'])#to fax data use Get
+    val2 = int(request.POST['num2'])#to submit data usew Post
     res = val1 + val2
     return render(request,'result.html',{'result':res})
--- sahan/sahan/settings.py
+++ sahan/sahan/settings.py
@@ -113,13 +113,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-APPEND_SLASH = False
+
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
