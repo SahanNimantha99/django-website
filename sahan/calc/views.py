b45a1e4bab4085bb906710e56fb5f2a5515fd8d4
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-09-19 08:42:22 +0530

    Update app

--- sahan/calc/views.py
+++ sahan/calc/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 def add(request):
 
-    val1 = request.GET['num1']
-    val2 = request.GET['num2']
-    res = int(val1) + int(val2)
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
     return render(request,'result.html',{'result':res})
